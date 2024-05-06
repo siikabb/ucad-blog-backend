bc4870d277f3ad69d5cb5845034befb5cb91a947
Joona Loukkola <111729194+siikabb@users.noreply.github.com>
2024-05-06 16:31:09 +0300

    add validation and update readme

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # A simple blog backend
 
-A replacing assignment for the UCAD course.
+a simple blog backend to replace missed assignments
+
+too lazy to provide the db script or schema so here is a pic of the creation
+![db](./db.png)
+
+the instructions mentioned that the data should be validated, but if every user input is a string, what is there to validate?
+gave the title and author a max length just in case and some basic sanitization
--- src/routes/blog-router.mjs
+++ src/routes/blog-router.mjs
@@ -6,11 +6,29 @@ import {
   postEntry,
   putEntry,
 } from '../controllers/blog-controller.mjs';
+import {body} from 'express-validator';
 
 const blogRouter = express.Router();
 
-blogRouter.route('/').get(getEntries).post(postEntry);
+blogRouter
+  .route('/')
+  .get(getEntries)
+  .post(
+    body('title').notEmpty().isLength({max: 255}).escape().trim(),
+    body('content').notEmpty().escape().trim(),
+    body('author').notEmpty().isLength({max: 255}).escape().trim(),
+    postEntry
+  );
 
-blogRouter.route('/:id').get(getEntryById).put(putEntry).delete(deleteEntry);
+blogRouter
+  .route('/:id')
+  .get(getEntryById)
+  .put(
+    body('title').isLength({max: 255}).escape().trim(),
+    body('content').escape().trim(),
+    body('author').isLength({max: 255}).escape().trim(),
+    putEntry
+  )
+  .delete(deleteEntry);
 
 export {blogRouter};
